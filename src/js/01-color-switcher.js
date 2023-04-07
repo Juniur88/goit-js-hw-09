b3d68f38a3a4b56e030a496d48159f63e89e7d9c
Juniur88 <114404621+Juniur88@users.noreply.github.com>
2023-04-07 15:13:22 +0300

    all

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,17 +7,14 @@ const refs = {
     body: document.querySelector('body'),
 }
 
-// refs.btnStop.setAttribute('disabled', true);
 refs.btnStop.disabled = true;
 refs.btnStart.addEventListener('click', onBtnStartChangeColor);
 refs.btnStop.addEventListener('click', onBtnStopChangeColor);
 
-
 function onBtnStartChangeColor() {
     refs.btnStart.disabled = true;
     refs.btnStop.disabled = false;
-    // refs.btnStart.setAttribute('disabled', true);
-    // refs.btnStop.removeAttribute('disabled');
+
 
     idInt = setInterval(() => {
         refs.body.style.backgroundColor = getRandomHexColor()
@@ -26,13 +23,11 @@ function onBtnStartChangeColor() {
 
 function onBtnStopChangeColor() {
     refs.btnStart.disabled = false;
-    refs.btnStop.disabled = true;
-    // refs.btnStart.removeAttribute('disabled');
-    // refs.btnStop.setAttribute('disabled', true);
+    refs.btnStart.disabled = true;
 
     clearInterval(idInt);
 }
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
